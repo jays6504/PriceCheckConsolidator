b17201cd84e42aef9d99ba0fec578f77c4a279fb
James Leahy Stephens <jaystephens1124@gmail.com>
2025-04-22 13:18:18 -0400

    Working on fetching

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Price Search Tool">
+      <h1>Welcome to the Price Search Tool</h1>
+      <p>
+        This is a simple price search tool that allows you to search for prices of various products.
+        You can enter a product name and the tool will return the current price of that product.
+      </p>
+      <label>
+        Product Name:
+        <input type="text" name="productName" />
+      </label>
+      <input type="submit" value="Search" onClick={basicAPICall} />
       </header>
     </div>
+      
   );
+}
+
+async function basicAPICall() {
+  var responseData = await fetch('https://api.sampleapis.com/wines/reds')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    var jsonData = response.json();
+    console.log(jsonData);
+    console.log(jsonData[0]);
+    return jsonData
+  })
 }
 
 export default App;
